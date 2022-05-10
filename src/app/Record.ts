f54fabe4279a90a70c0c45aa0e9b67a428115c70
jengworks <jengworks10@gmail.com>
2022-05-10 10:42:12 -0400

    uploading site with nav buttons

--- src/app/Record.ts
+++ src/app/Record.ts
@@ -1,10 +1,16 @@
 export interface Record {
-    action: string,
-    completed: boolean,
+    completed: boolean,    
+    rolled: boolean,
+    action: string,        //  sto, stc, bto, btc, rolled, dividend //
     date: string,
     ticker: string,
     strikeDate: string,
     strikePrice: number,
     contracts: number,
     price: number,
+    tickerTwo?: string,    //  These fields are for 2nd opening record for rolling
+    strikeDateTwo?: string,
+    strikePriceTwo?: number,
+    contractsTwo?: number,
+    priceTwo?: number,
 }
